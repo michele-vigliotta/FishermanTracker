e0a5a4f1e1b9e768e85572539e9b3c09e2bd4fe4
michele-vigliotta <michele.vigliotta00@gmail.com>
2025-11-05 12:06:30 +0100

    refactoring file structure

--- scripts/manual_test.py
+++ scripts/manual_test.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 model = YOLO('best_cls.pt')
 
-image_path = 'test4.jpg'
+image_path = 'pics/test4.jpg'
 
 results = model(image_path, conf=0.33)
 results[0].show()  # mostra l'immagine con classification
